272a9e76aabe20f73773d61d4378e954cbf8f093
AneVG <81672333+AneVG@users.noreply.github.com>
2024-02-10 22:20:52 +0200

    repo-init

---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group"
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "The location of  theresource group"
+}
+
+variable "app_service_plan_name" {
+  type        = string
+  description = "The app service plan name"
+}
+
+variable "app_service_name" {
+  type        = string
+  description = "The app service name"
+}
+
+variable "sql_server_name" {
+  type        = string
+  description = "The name of the SQL server"
+}
+
+variable "sql_database_name" {
+  type        = string
+  description = "The name of database"
+}
+
+variable "sql_admin_login" {
+  type        = string
+  description = "The username for the sql server"
+}
+
+variable "sql_admin_password" {
+  type        = string
+  description = "The username for the sql suser"
+}
+
+variable "firewall_rule_name" {
+  type        = string
+  description = "The name of the firewall ruler"
+}
+
+variable "repo_URL" {
+  type        = string
+  description = "The URL of the GitHub repo"
+}
+
