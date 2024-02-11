80e8970aef5404f57f471a030c8f4836b2cd3ab7
AneVG <81672333+AneVG@users.noreply.github.com>
2024-02-11 15:25:57 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,12 @@
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group"
-  default     = "my_resource_group" 
-}
+  default     = "TaskBoardRG" 
+}   
 
 variable "resource_group_location" {
   type        = string
-  description = "The location of the resource group"
-  default     = "westeurope"
+  description = "The location of  theresource group"
 }
 
 variable "app_service_plan_name" {
@@ -27,25 +26,26 @@ variable "sql_server_name" {
 
 variable "sql_database_name" {
   type        = string
-  description = "The name of the database"
+  description = "The name of database"
 }
 
 variable "sql_admin_login" {
   type        = string
-  description = "The username for the SQL server"
+  description = "The username for the sql server"
 }
 
 variable "sql_admin_password" {
   type        = string
-  description = "The password for the SQL server"
+  description = "The username for the sql suser"
 }
 
 variable "firewall_rule_name" {
   type        = string
-  description = "The name of the firewall rule"
+  description = "The name of the firewall ruler"
 }
 
 variable "repo_URL" {
   type        = string
-  description = "The URL of the GitHub repository"
+  description = "The URL of the GitHub repo"
 }
+
