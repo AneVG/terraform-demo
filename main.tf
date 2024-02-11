0642dec2ef0c7b7f62efa5af474caa4a9da0ffa1
AneVG <81672333+AneVG@users.noreply.github.com>
2024-02-11 16:58:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "StorageRG"
     storage_account_name = "taskboardstorage"
-    contaicontainer_name = "taskboardcontainer"
+    container_name = "taskboardcontainer"
     key                  = "terraform.tfstate"
   }
 }
